22a9c350c2ec1ca7e4f289c142f34ab17c440f18
Milena Petrovic <92889847+milenapetrov@users.noreply.github.com>
2024-09-11 13:01:58 -0400

    Added excel file with path accuracy values across all environments for each participant. This file was derived from the nav training analysis repository, plot_nav_paths_2.py.

--- navigate.r
+++ navigate.r
@@ -7,7 +7,8 @@ library(openxlsx)
 # print(x) 
 
 #data <- read.csv("/Users/milenapetrovic/UFL Dropbox/Milena Petrovic/dataCopy/processed_data/training/navigation_training_processed/main_data/nav_training_main_data_2024_05_30.csv", header = TRUE, sep = ",") 
-df <- read_excel("/Users/milenapetrovic/Downloads/nav-training-analysis-2/nav_metrics.xlsx")
+#df <- read_excel("/Users/milenapetrovic/Downloads/nav-training-analysis-2/nav_metrics.xlsx")
+df <- read_excel("nav_metrics.xlsx")
 
 df$Environment1 <- as.factor(grepl("A", df$Environment))
 df$Environment2 <- as.factor(grepl("B", df$Environment))
@@ -16,6 +17,7 @@ df$Environment4 <- as.factor(grepl("C", df$Environment))
 df$`Path Accuracy` <- as.factor(df$`Path Accuracy`)
 
 df_env <- df[, c("Environment1", "Environment2", "Environment3", "Environment4", "Path Accuracy")]
+
 # Define the model formula
 f <- cbind(Environment1, Environment2, Environment3, Environment4,`Path Accuracy`) ~ 1
 
